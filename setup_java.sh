0154d04ece446cbba666f5daf490222ee348a480
Anbu Ramasamy <5172759+ZeroProbability@users.noreply.github.com>
2018-11-12 00:18:55 +0000

    set java version to 8

--- setup_java.sh
+++ setup_java.sh
@@ -3,7 +3,7 @@
 apt-get install -y libxtst6
 
 su -c 'curl -s "https://get.sdkman.io" | bash' anbu
-su -c 'source "/home/anbu/.sdkman/bin/sdkman-init.sh" && sdk install java' anbu
+su -c 'source "/home/anbu/.sdkman/bin/sdkman-init.sh" && sdk install java 8.0.181-zulu' anbu
 su -c 'source "/home/anbu/.sdkman/bin/sdkman-init.sh" && sdk install groovy' anbu
 su -c 'source "/home/anbu/.sdkman/bin/sdkman-init.sh" && sdk install kotlin' anbu
 su -c 'source "/home/anbu/.sdkman/bin/sdkman-init.sh" && sdk install gradle' anbu
